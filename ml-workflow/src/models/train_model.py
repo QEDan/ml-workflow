86478093c685c4d860bf1031e97b5a224b5dd48f
QEDan <danpmazur@gmail.com>
2019-03-28 17:41:05 -0700

    Updates

--- ml-workflow/src/models/train_model.py
+++ ml-workflow/src/models/train_model.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
 from sklearn.preprocessing import LabelEncoder
+import yaml
+
+
+def load_configs():
+    with open('configs/train_configs.yaml', 'r') as f:
+        configs = yaml.load(f)
+    return configs
 
 
 def prepare_X(df):
@@ -13,14 +21,21 @@ def prepare_X(df):
     for feature in nonnumeric_columns:
         X[feature] = le.fit_transform(X[feature])
 
-    X = X.as_matrix()
+    X = X.values
     return X
 
 
-def train_model(df):
+def train_model(df, configs):
     train_X = prepare_X(df)
     train_y = df['Survived']
-    clf = RandomForestClassifier(max_depth=3, n_estimators=300)
+    if configs.get('model_type') == 'RandomForest':
+        max_depth = configs.get('RandomForest').get('max_depth')
+        n_estimators = configs.get('RandomForest').get('n_estimators')
+        clf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    elif configs.get('model_type') == 'GradientBoosting':
+        max_depth = configs.get('GradientBoosting').get('max_depth')
+        n_estimators = configs.get('GradientBoosting').get('n_estimators')
+        clf = GradientBoostingClassifier(max_depth=max_depth, n_estimators=n_estimators)
     model = clf.fit(train_X, train_y)
     return model
 
@@ -33,9 +48,11 @@ def load_data():
 
 if __name__ == "__main__":
     train_df, test_df = load_data()
-    model = train_model(train_df)
+    configs = load_configs()
+    model = train_model(train_df, configs.get('training'))
     test_X = prepare_X(test_df)
     predictions = model.predict(test_X)
     submission = pd.DataFrame({'PassengerId': test_df['PassengerId'],
                                'Survived': predictions})
     submission.to_csv("data/processed/submission.csv", index=False)
+    print("Done")
